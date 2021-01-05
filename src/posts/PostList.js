6e32d8ce099684e0fbd3ef1d89ca1067c9d30635
Shabix <kmharouna@gmail.com>
2021-01-05 12:25:30 +0300

    Displaying JSON data in a component

---
+++ src/posts/PostList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import PostData from '../data/posts.json'
+
+class PostList extends Component {
+ 
+  render () {
+    return (
+      <div >
+        <h1>Hello There</h1>
+        {PostData.map((PostDetail, index) => {
+          return<div>
+            <h1>{PostDetail.title}</h1>
+            <p>{PostDetail.content}</p>
+          </div> 
+        })}
+      </div>
+    )
+  }
+}
+
+export default PostList
